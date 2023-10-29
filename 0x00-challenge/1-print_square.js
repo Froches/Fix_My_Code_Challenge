0718c870c06f206347daede4579fb59c69c6eb73
Froches <michaelfavour418@gmail.com>
2023-10-29 09:05:55 -0700

    fixed?

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,25 +6,24 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-const size = parseInt(process.argv[2])
+const size = parseInt(process.argv[2]);
 
 if (isNaN(size) || size <= 0) {
-	process.stderr.write("Invalid size. Please provide a positive integer.\n")
-	process.exit(1);
+    process.stderr.write("Invalid size. Please provide a positive integer.\n");
+    process.exit(1);
 }
 
 for (let i = 0; i < size; i++) {
-	let row = "";
-	for (let j = 0; j < size; j++) {
-		row += "#";
-	}
-	console.log(row);
+    let row = "";
+    for (let j = 0; j < size; j++) {
+        row += "#";
+    }
+    console.log(row);
 }
